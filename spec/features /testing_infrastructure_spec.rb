cf6b3698740c1ea34e5b8cf3fbbf264beb845815
ianmcnicholas <ianjmcnicholas@gmail.com>
2021-02-17 17:33:24 +0000

    Initial form works and sends user to 'name' page

--- spec/features /testing_infrastructure_spec.rb
+++ spec/features /testing_infrastructure_spec.rb
@@ -2,16 +2,16 @@ require 'app'
 feature "testing infrastructure" do
   scenario "runs app and check page content" do
     visit '/'
-    expect(page).to have_content 'Testing infrastructure'
+    expect(page).to have_content 'Prepare for battle!'
   end
 end
 
 feature "enter player names" do
   scenario "returns entered names on the screen" do
     visit '/'
-    fill_in('Player 1 Name', with: 'Ian')
-    fill_in('Player 2 Name', with: 'Anna')
+    fill_in('name1', with: 'Ian')
+    fill_in('name2', with: 'Anna')
     click_button('Submit')
-    expect(page).to have_content "Ian vs Anna"
+    expect(page).to have_content "The next fight is between Ian and Anna"
   end
 end
